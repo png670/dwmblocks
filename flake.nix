8a0d38e6f88b974ebe42edea0741ee580218cc4f
png670 <png6760@gmail.com>
2025-02-18 15:41:39 +0000

    test

--- flake.nix
+++ flake.nix
@@ -1,12 +1,27 @@
 
 {
-  description = "DWMBlocks flake";
+  description = "DWMBlocks (Status Bar for DWM)";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
-
-  outputs = { self, nixpkgs }: {
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.dwmblocks.overrideAttrs (old: {
-      src = ./.;
-    });
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.11";
   };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      packages.${system}.default = pkgs.stdenv.mkDerivation {
+        name = "dwmblocks";
+        src = /path/to/your/dwmblocks;  # Use the local path to your dwmblocks source
+
+        buildInputs = [ pkgs.xorg.libX11 pkgs.xorg.libXft pkgs.xorg.libXinerama ];
+
+        installPhase = ''
+          mkdir -p $out/bin
+          make PREFIX=$out install
+        '';
+      };
+    };
 }
